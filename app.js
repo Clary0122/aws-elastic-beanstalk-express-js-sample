e06db8124962df7a3b10ac237935d4837772f766
Clary <79209568+Clary0122@users.noreply.github.com>
2022-06-07 14:09:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.use(express.static('public'));
-app.get('/', (req, res) => res.sendfile("./index.html"));
+app.get('/', (req, res) => res.sendfile("index.html"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
